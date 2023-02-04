d9052312724635d4ac47ab4b8625a91eb8d77584
Ruslan <123066002+Toltek1320@users.noreply.github.com>
2023-02-04 21:25:51 +0300

    Update homework_3586_3.cs

--- homework_3586_3.cs
+++ homework_3586_3.cs
@@ -3,7 +3,6 @@
 // 23432 -> да
 // 12821 -> да
 
-
 System.Console.Write("Введите пятизначное число: ");
 int num = int.Parse(Console.ReadLine());
 if (num > 9999 && num < 100000)
@@ -20,4 +19,46 @@ if (num > 9999 && num < 100000)
 else
 {
     System.Console.WriteLine("Это не пятизначное число!");
+}
+
+// ДЗ_21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3, 6, 8); B (2, 1, -7) -> 15,84
+// A (7, -5, 0); B (1, -1, 9) -> 11,53
+
+System.Console.Write("Enter number X1: ");
+double numX1 = double.Parse(Console.ReadLine());
+
+System.Console.Write("Enter number Y1: ");
+double numY1 = double.Parse(Console.ReadLine());
+
+System.Console.Write("Enter number Z1: ");
+double numZ1 = double.Parse(Console.ReadLine());
+
+System.Console.Write("Enter number X2: ");
+double numX2 = double.Parse(Console.ReadLine());
+
+System.Console.Write("Enter number Y2: ");
+double numY2 = double.Parse(Console.ReadLine());
+
+System.Console.Write("Enter number Z2: ");
+double numZ2 = double.Parse(Console.ReadLine());
+
+double numSQRT = Math.Pow((numX1 - numX2),2) + Math.Pow((numY1 - numY2),2) + Math.Pow((numZ1 - numZ2),2); 
+// Math.Pow((numX1 - numX2),2) Возведение в степень
+double result = Math.Sqrt(numSQRT); 
+// Math.Sqrt Корень
+
+Console.WriteLine(result.ToString("c2")); 
+
+// ДЗ_23 Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N
+
+System.Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+int I = 1;
+
+while(I <= num)
+{
+    System.Console.Write(Math.Pow(I, 3) + " ");
+    I += 1;
 }
